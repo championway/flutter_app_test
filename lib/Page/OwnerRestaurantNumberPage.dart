b3e2753cfd786e62fef10e23008460b2f7386bf1
championway <ccpwearth@gmail.com>
2022-06-15 00:55:43 +0800

    update Owner Restaurant Number Page

--- lib/Page/OwnerRestaurantNumberPage.dart
+++ lib/Page/OwnerRestaurantNumberPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_test/Util/Util.dart';
+
+//https://www.youtube.com/watch?v=QBwLJKbCIlo&ab_channel=MadewithFlutter
 
 class OwnerRestaurantNumberPage extends StatefulWidget {
   @override
@@ -7,13 +10,53 @@ class OwnerRestaurantNumberPage extends StatefulWidget {
 }
 
 class _OwnerRestaurantNumberPageState extends State<OwnerRestaurantNumberPage> {
+//  final Function(int) onNumberSelected;
+//
+//  NumericPad({@required this.onNumberSelected});
+  String numberPadValue = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("候位號碼牌系統"),
       ),
-      body: numberPad(),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  numberPadValue,
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Row(
+              children: [
+                Expanded(flex: 6, child: numberPad()),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      buildAction(type: 0),
+                      buildAction(type: 1),
+                      buildAction(type: 2),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -76,10 +119,47 @@ class _OwnerRestaurantNumberPageState extends State<OwnerRestaurantNumberPage> {
     );
   }
 
-  Widget buildNumber({int? number, int type : 0}) {
+  onNumberSelected(value) {
+    setState(() {
+      if (value >= 0) {
+        if (numberPadValue.length + 1 <= 4) {
+          numberPadValue = numberPadValue + value.toString();
+        }
+      } else if (value == -1) {
+        numberPadValue = numberPadValue.substring(0, numberPadValue.length - 1);
+      } else if (value == -2) {
+        numberPadValue = "";
+      }
+    });
+  }
+
+  onActionSelected(value) {
+    setState(() {
+      int numberPadValueInt;
+      if (numberPadValue == "") {
+        numberPadValueInt = 0;
+      } else {
+        numberPadValueInt = int.parse(numberPadValue);
+      }
+      if (value == 1) {
+        if (numberPadValueInt < 9999) {
+          numberPadValue = (numberPadValueInt + 1).toString();
+        }
+      } else if (value == -1) {
+        if (numberPadValueInt > 0) {
+          numberPadValue = (numberPadValueInt - 1).toString();
+        }
+      } else if (value == 0) {
+        showToast("Send");
+      }
+    });
+  }
+
+  Widget buildNumber({int? number, int type: 0}) {
     // type = 0 : number, type = 1 : backspace, type = 2 : clear
     Widget content = Container();
-    if (type == 0){
+    late Function onTapFunc;
+    if (type == 0) {
       content = Text(
         number!.toString(),
         style: TextStyle(
@@ -87,23 +167,103 @@ class _OwnerRestaurantNumberPageState extends State<OwnerRestaurantNumberPage> {
           fontWeight: FontWeight.bold,
         ),
       );
-    }
-    else if (type == 1){
-      content = Icon(Icons.backspace, size: 28,);
-    }
-    else if (type == 2){
-      content = Icon(Icons.clear, size: 28,);
+      onTapFunc = () {
+        onNumberSelected(number);
+      };
+    } else if (type == 1) {
+      content = Icon(
+        Icons.backspace,
+        size: 28,
+      );
+      onTapFunc = () {
+        onNumberSelected(-1);
+      };
+    } else if (type == 2) {
+      content = Icon(
+        Icons.clear,
+        size: 28,
+      );
+      onTapFunc = () {
+        onNumberSelected(-2);
+      };
     }
     return Expanded(
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.black12,
-            borderRadius: BorderRadius.all(Radius.circular(15)),
+        padding: const EdgeInsets.all(4.0),
+        child: InkWell(
+          customBorder: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
           ),
-          child: Center(
-            child: content,
+          onTap: () {
+            onTapFunc();
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            child: Center(
+              child: content,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildAction({int type: 0}) {
+    // type = 0 : number, type = 1 : backspace, type = 2 : clear
+    Widget content = Container();
+    late Function onTapFunc;
+    if (type == 0) {
+      content = Text(
+        "+1",
+        style: TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+        ),
+      );
+      onTapFunc = () {
+        onActionSelected(1);
+      };
+    } else if (type == 1) {
+      content = Text(
+        "-1",
+        style: TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+        ),
+      );
+      onTapFunc = () {
+        onActionSelected(-1);
+      };
+    } else if (type == 2) {
+      content = Icon(
+        Icons.send,
+        size: 28,
+      );
+      onTapFunc = () {
+        onActionSelected(0);
+      };
+    }
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: InkWell(
+          customBorder: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          onTap: () {
+            onTapFunc();
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            child: Center(
+              child: content,
+            ),
           ),
         ),
       ),
