cf5076bb090c68ac1f7fb8eec3303e4d1fa84c5f
championway <ccpwearth@gmail.com>
2023-01-26 12:02:53 +0800

    add image picker select function

--- lib/Page/ImagePickerPage.dart
+++ lib/Page/ImagePickerPage.dart
@@ -1,23 +1,29 @@
-
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_app_test/Util/Util.dart';
 import 'package:flutter_storage_path/flutter_storage_path.dart';
-// ignore: import_of_legacy_library_into_null_safe
-//import 'package:storage_path/storage_path.dart';
 
 class FileModel {
-  List<String> files = [];
+  List<String> filePathList = [];
   String folder = "";
 
-  FileModel({required this.files, required this.folder});
+  FileModel({required this.filePathList, required this.folder});
 
   FileModel.fromJson(Map<String, dynamic> json) {
-    files = json['files'].cast<String>();
+    filePathList = json['files'].cast<String>();
     folder = json['folderName'];
   }
+}
+
+class ImageSelectModel {
+  ImageSelectModel(
+      {required this.path, this.isSelect: false, this.selectOrder: 0});
+
+  String path;
+  bool isSelect;
+  int selectOrder; // 0 --> not selected
 }
 
 class ImagePickerPage extends StatefulWidget {
@@ -30,10 +36,15 @@ class ImagePickerPage extends StatefulWidget {
 }
 
 class _ImagePickerPageState extends State<ImagePickerPage> {
-  List<FileModel> files = [];
-  FileModel? selectedModel;
-  String? image;
-  List<String> _selectedList = [];
+  static const int MAX_SELECT_NUM = 7;
+  List<FileModel> _allFileList = [];
+  FileModel? _fileListModel;
+  String? _currentImagePath;
+
+  List<String> _selectedPathList = [];
+//  List<ImageSelectModel> _selectedImageList = [];
+//  int _totalSelectedOrder = 0;
+
   @override
   void initState() {
     super.initState();
@@ -43,12 +54,59 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
   getImagesPath() async {
     var imagePath = await StoragePath.imagesPath;
     var images = jsonDecode(imagePath!) as List;
-    files = images.map<FileModel>((e) => FileModel.fromJson(e)).toList();
-    if (files.length > 0)
+    _allFileList = images.map<FileModel>((e) => FileModel.fromJson(e)).toList();
+    if (_allFileList.length > 0) {
+//      for (String path in _allFileList[0].filePathList) {
+//        if (_selectedPathList.contains(path)) {
+//          for (int i = 0; i < _se.length; i++) {
+//            if (_imageList[i].path == path) {
+//              _imageList
+//            }
+//          }
+//        }
+//      }
       setState(() {
-        selectedModel = files[0];
-        image = files[0].files[0];
+        _fileListModel = _allFileList[0];
+        _currentImagePath = _allFileList[0].filePathList[0];
       });
+    }
+  }
+
+  Widget imageThumbnailWidget(String path) {
+    int order = 0;
+    bool isSelect = _selectedPathList.contains(path);
+    if (isSelect) order = _selectedPathList.indexOf(path) + 1;
+    return Stack(
+      children: [
+        Image.file(
+          File(path),
+          fit: BoxFit.cover,
+        ),
+        Visibility(
+            visible: isSelect,
+            child: Positioned(right: 3, top: 3, child: Text("$order")))
+      ],
+    );
+  }
+
+  void selectedImageFunction(String path) {
+    if (!_selectedPathList.contains(path)) {
+      // if this image haven't been selected
+      if (_selectedPathList.length >= MAX_SELECT_NUM) {
+        // already select maximum amount
+        showToast("上限是$MAX_SELECT_NUM張照片");
+        return;
+      }
+      setState(() {
+        _selectedPathList.add(path);
+      });
+    } else {
+      // if this image already been selected
+      setState(() {
+        _selectedPathList.remove(path);
+//        _selectedImageList.removeWhere((element) => element.path == path);
+      });
+    }
   }
 
   @override
@@ -63,23 +121,25 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
                 Row(
                   children: <Widget>[
                     IconButton(
-                      onPressed: (){Navigator.pop(context);},
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
                       icon: Icon(Icons.clear),
                     ),
                     SizedBox(width: 10),
                     DropdownButtonHideUnderline(
                         child: DropdownButton<FileModel>(
-                          items: getItems(),
-                          onChanged: (FileModel? d) {
-                            if (d == null) return;
-                            assert(d.files.length > 0);
-                            image = d.files[0];
-                            setState(() {
-                              selectedModel = d;
-                            });
-                          },
-                          value: selectedModel,
-                        ))
+                      items: getItems(),
+                      onChanged: (FileModel? d) {
+                        if (d == null) return;
+                        assert(d.filePathList.length > 0);
+                        _currentImagePath = d.filePathList[0];
+                        setState(() {
+                          _fileListModel = d;
+                        });
+                      },
+                      value: _fileListModel,
+                    ))
                   ],
                 ),
                 Padding(
@@ -87,10 +147,13 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
                   child: IconButton(
                     padding: EdgeInsets.zero,
                     constraints: BoxConstraints(),
-                    onPressed: (){
-                      print(_selectedList);
+                    onPressed: () {
+//                      print(_selectedList);
                     },
-                    icon: Icon(Icons.check, color: Colors.blue,),
+                    icon: Icon(
+                      Icons.check,
+                      color: Colors.blue,
+                    ),
                   ),
                 )
               ],
@@ -98,40 +161,38 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
             Divider(),
             Container(
                 height: MediaQuery.of(context).size.height * 0.45,
-                child: image != null
-                    ? Image.file(File(image!),
-                    height: MediaQuery.of(context).size.height * 0.45,
-                    width: MediaQuery.of(context).size.width)
+                child: _currentImagePath != null
+                    ? Image.file(File(_currentImagePath!),
+                        height: MediaQuery.of(context).size.height * 0.45,
+                        width: MediaQuery.of(context).size.width)
                     : Container()),
             Divider(),
-            selectedModel == null || selectedModel!.files.length < 1
+            _fileListModel == null ||
+                    _fileListModel!.filePathList.length < 1 // TODO
                 ? Container()
                 : Container(
-              height: MediaQuery.of(context).size.height * 0.38,
-              child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4,
-                      crossAxisSpacing: 4,
-                      mainAxisSpacing: 4),
-                  itemBuilder: (_, i) {
-                    var file = selectedModel!.files[i];
-                    return GestureDetector(
-                      child: Image.file(
-                        File(file),
-                        fit: BoxFit.cover,
-                      ),
-                      onTap: () {
-                        setState(() {
-                          image = file;
-                        });
-                      },
-                      onLongPress: (){
-                        _selectedList.add(file);
-                      },
-                    );
-                  },
-                  itemCount: selectedModel!.files.length),
-            )
+                    height: MediaQuery.of(context).size.height * 0.38,
+                    child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                            crossAxisSpacing: 4,
+                            mainAxisSpacing: 4),
+                        itemBuilder: (_, i) {
+                          String path = _fileListModel!.filePathList[i];
+                          return GestureDetector(
+                            child: imageThumbnailWidget(path),
+                            onTap: () {
+                              setState(() {
+                                _currentImagePath = path;
+                              });
+                            },
+                            onLongPress: () {
+                              selectedImageFunction(path);
+                            },
+                          );
+                        },
+                        itemCount: _fileListModel!.filePathList.length),
+                  )
           ],
         ),
       ),
@@ -139,14 +200,14 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
   }
 
   List<DropdownMenuItem<FileModel>> getItems() {
-    return files
+    return _allFileList
         .map((e) => DropdownMenuItem(
-      child: Text(
-        e.folder,
-        style: TextStyle(color: Colors.black),
-      ),
-      value: e,
-    ))
+              child: Text(
+                e.folder,
+                style: TextStyle(color: Colors.black),
+              ),
+              value: e,
+            ))
         .toList();
   }
 }
